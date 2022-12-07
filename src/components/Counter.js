83a9a805c30cd1ad2fa3441fe11c4ffd73364b87
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2022-12-07 09:37:38 +0300

    add increment by amount action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement } from "../redux/counter/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementByAmount
+} from "../redux/counter/counterSlice";
 
 export default function Counter() {
   const counterValue = useSelector((state) => state.counter.value);
@@ -10,6 +14,9 @@ export default function Counter() {
       <h1>{counterValue}</h1>
       <button onClick={() => dispatch(decrement())}>decrement</button>
       <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>
+        incrementByAmount
+      </button>
     </div>
   );
 }
