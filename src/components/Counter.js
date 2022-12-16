dc2654e17bc9bf69d18c5d7800018d02ce62293d
Emir Mertoğlu <emir.mertoglu.97@gmail.com>
2022-12-16 09:42:18 +0300

    fix increment by amount function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   increment,
@@ -6,6 +7,7 @@ import {
 } from "../redux/counter/counterSlice";
 
 export default function Counter() {
+  const [amount, setAmount] = useState(3);
   const counterValue = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
@@ -14,7 +16,13 @@ export default function Counter() {
       <h1>{counterValue}</h1>
       <button onClick={() => dispatch(decrement())}>decrement</button>
       <button onClick={() => dispatch(increment())}>increment</button>
-      <button onClick={() => dispatch(incrementByAmount(5))}>
+      <br /> <br />
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <button onClick={() => dispatch(incrementByAmount(amount))}>
         incrementByAmount
       </button>
     </div>
